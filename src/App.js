6f573dd9e37100866ded4a29e5d2adbd6dcffc66
Ugo <ugo.prenat@gmail.com>
2022-02-05 18:10:23 +0100

    temp page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import './styles/index.scss'
+
+import { Github } from './assets/svg/Github';
 
 function App() {
   return (
     <div className="App">
-      Genely
+      <div className='container'>
+        <h1>Genely</h1>
+        <p>La plateforme est en cours de développement...</p>
+      </div>
+      
+      <footer>
+        <a href="https://dev.genely.dev/">Accéder à la version dev</a>
+        <a href='https://github.com/ugo-prenat/genely/tree/dev'>
+          <Github />
+        </a>
+      </footer>
     </div>
   );
 }
