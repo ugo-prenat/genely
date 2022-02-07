d4c587bd01fac80d4a2658f6757e98d19b88ecbb
Ugo <ugo.prenat@gmail.com>
2022-02-07 21:56:09 +0100

    header done

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom'
 
+import Logout from '../assets/svg/Logout';
+import Profil from '../assets/svg/Profil';
+import Settings from '../assets/svg/Settings';
+import Question from '../assets/svg/Question';
+
 import '../styles/header.scss'
 
 export default function Header() {
   const [isAuth, setIsAuth] = useState(true);
-  const [showDropdown, setShowDropdown] = useState(true);
+  const [showDropdown, setShowDropdown] = useState(false);
   
   const tempUsername = 'ougo'
   const tempPP = 'https://rocket-league.com/content/media/users/avatar/600px/5d587182eb1640995184.png'
@@ -21,12 +26,12 @@ export default function Header() {
         <Link to='/new-component'>
           <p className='second-btn'>+ nouveau composant</p>
         </Link>
-        <Link to={tempUsername}>
-          <div className='pp-container' onMouseEnter={() => setShowDropdown(true)} onMouseLeave={() => setShowDropdown(false)}>
+        <div className='pp-container' onMouseEnter={() => setShowDropdown(true)} onMouseLeave={() => setShowDropdown(false)}>
+          <Link to={tempUsername}>
             <img src={tempPP} alt='Profil' />
-          </div>
-        </Link>
-        { showDropdown && <Dropdown username={tempUsername} /> }
+          </Link>
+          { showDropdown && <Dropdown username={tempUsername} hideDropdown={() => setShowDropdown(false)} /> }
+        </div>
       </div>
       :
       <div>pas auth</div>
@@ -37,9 +42,21 @@ export default function Header() {
 
 function Dropdown(props) {
   return <div className='dropdown'>
-    <Link to={props.username}>Profil</Link>
-    <Link to='/settings'>Paramètres</Link>
-    <Link to='/about'>A propos</Link>
-    <Link to='/'>Déconnexion</Link>
+    <Link to={props.username} onClick={() => props.hideDropdown()}>
+      <Profil />
+      Profil
+    </Link>
+    <Link to='/settings' onClick={() => props.hideDropdown()}>
+      <Settings />
+      Paramètres
+    </Link>
+    <Link to='/about' onClick={() => props.hideDropdown()}>
+      <Question />
+      A propos
+    </Link>
+    <Link to='/' onClick={() => props.hideDropdown()}>
+      <Logout />
+      Déconnexion
+    </Link>
   </div>
 }
